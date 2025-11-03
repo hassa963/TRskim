4b3488cf1ad0ad36e611f2fe20f458eadf681b04
Nour Hassan <nour.hassan@mail.utoronto.ca>
2025-11-02 19:09:23 -0500

    finished encodeTR tests

---
+++ tests/testthat/test-test-encodeTR.R
@@ -0,0 +1,16 @@
+
+test_that("encodeTR correctly encodes decomposed motifs", {
+  decomposed_TR <- c("AC", "AC", "GT", "AC")
+  motifs <- c("AC", "GT")
+
+  encoded <- encodeTR(decomposed_TR, motifs)
+
+  # Expected outputs
+  expected_encoded <- "AABA"
+  expected_map <- c("A", "B")
+  names(expected_map) <- c("AC", "GT")
+
+  expect_named(encoded, c("encoded", "map"))
+  expect_equal(encoded$encoded, expected_encoded)
+  expect_equal(encoded$map, expected_map)
+})
